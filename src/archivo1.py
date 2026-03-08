e9f2839f742e3d7e6e6f84ab409ba89fbc77bcac
pacholandin <pacholandin@gmail.com>
2026-03-08 18:20:35 +0100

    codigo

---
+++ src/archivo1.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import json
+
+def json_to_csv(file_path):
+    out = []
+    
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            # limpiar lineas
+            if line.strip() and (line.strip() == "[" or line.strip() == "]" or line.strip() == ""):
+                continue
+            else:
+                guardar = json.loads(line.strip())
+                
+               # nombre del conductor y su equipo
+                fila = {
+                    "Nombre": guardar.get("full_name"),
+                    "Scuderia": guardar.get("team_name")
+                }
+                out.append(fila)
+
+    # comprobar si existe el output directory
+    os.makedirs("data/clean", exist_ok=True)
+    
+  
+    fieldnames = ["Nombre", "Scuderia"]
+    
+    with open("data/clean/drivers_list.csv", "w", newline="", encoding="utf-8") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, extrasaction="ignore")
+        writer.writeheader()
+        writer.writerows(out)
+
+
+json_to_csv("data/raw/drivers_data.json")
---
+++ src/download.py
@@ -0,0 +1,21 @@
+import os
+import json
+import requests
+
+def api_request(url):
+    response = requests.get(url)
+    return response.json()
+
+def data_writing(file_path, data, mode="w"):
+    os.makedirs("data/raw", exist_ok=True)
+
+    with open(file_path, mode, encoding="utf-8") as f:
+        for element in data:
+            f.write(json.dumps(element) + "\n")
+
+# drivers
+drivers_url = "https://api.openf1.org/v1/drivers?session_key=latest"
+drivers_data = api_request(drivers_url)
+drivers_file_path = "data/raw/drivers_data.json"
+
+data_writing(drivers_file_path, drivers_data)
