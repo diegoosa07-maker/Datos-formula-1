9fe35f9dd711552203bc54e7ffaf3036422c92a3
Yesesesesesssssssss <166861002+Yesesesesesssssssss@users.noreply.github.com>
2026-03-09 11:05:47 +0100

    Archivo 3: Team_info.v1
    He optimizado un poco el código y eliminado el apartado de la IA, junto con más explicaciones del código.

--- src/Archivo 3.py
+++ src/Archivo 3.py
@@ -15,24 +15,14 @@ def data_writing(file_path, data,mode="w"):
     os.makedirs("data/raw", exist_ok=True)
 
     with open(file_path, mode,  encoding="utf-8") as f:
-        for element in data:
+        for element in data["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"]:
             f.write(json.dumps(element) + "\n")
 
         # La principal diferencia entre este código y el de clase, aparte de las url, es que no uso otro archivo registro POR AHORA.
-        print(f"Se guardaron {len(data)} elementos en {file_path}")
+        print(f"Se guardaron {len(data["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"])} elementos en {file_path}")
 
 # Las URL para que funcionen el programa:
 f1_url = "http://api.jolpi.ca/ergast/f1/current/constructorStandings.json"
 f1_data = api_request(f1_url)
+data_writing("data/raw/team_info.json", f1_data, "w")
 
-
-# Parte IA: Por algún motivo, mi programa parecía no ser capaz de crear el archivo, así que solicité la ayuda de la IA para esta parte.
-if f1_data:
-    # Navegamos por el JSON para llegar a la lista de puntos
-    # La ruta es: MRData -> StandingsTable -> StandingsLists[primer elemento] -> ConstructorStandings
-    standings_list = f1_data["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"]
-    
-    f1_file_path = "data/raw/team_info.json"
-    
-    # Guardamos los datos
-    data_writing(f1_file_path, standings_list)
